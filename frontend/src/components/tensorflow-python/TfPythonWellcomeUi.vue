4a10f4414698fb46aa8e4a6a7715f1350622d337
01401783 <a01401783@unet.univie.ac.at>
2019-03-01 08:41:44 +0100

    sidebar structure part2

---
+++ frontend/src/components/tensorflow-python/TfPythonWellcomeUi.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="bootstrap">
+
+    <h1 style="color: red;">Description page of TensorFlow Python will be created.</h1>
+  </div>
+
+</template>
---
+++ frontend/src/components/tensorflow-python/TfPythonWineUi.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="bootstrap">
+
+    <h3>Wine Classification clicked (python)</h3>
+
+  </div>
+
+</template>
